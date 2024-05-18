88e52b9fcd32b076220b0fc29ea23cb262da9926
Michal Trojanowski <mtroja98@gmail.com>
2024-05-18 21:20:38 +0200

    Map creation

--- src/main/java/com/irnin/games/mitria/entity/Map.java
+++ src/main/java/com/irnin/games/mitria/entity/Map.java
@@ -1,4 +1,5 @@
 package com.irnin.games.mitria.entity;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Map {
@@ -13,44 +14,51 @@ public class Map {
     public Map() {
         int[][] map_array = new int[map_width][map_height];
         this.map_array = map_array;
-
-
-
     }
 
-
-
     public void drawMap() {
-        if (Map.getMapInstance() == null) {
-            System.out.println("getMapInstance  is null although everyone says it cannot be.");
-        }
-
-        int i = 0, j = 0, k = 0;
+        int counter_rows = 0, counter_columns = 0;
+        int position_in_row = 0, column_no = 0, k = 0;
         Random rand = new Random();
         int random_number_in_0_1 = rand.nextInt(2);
 
-        while (i < map_width-1) {
+        while (column_no < map_height) {
 
+          //  if ((column_no + block_offset) < map_height) {
 
-            while (j < map_height) {
+                while (position_in_row < map_width) {
 
-                if ((j += block_offset) < map_height) {
-                    j += block_offset;
-                    mapInstance.map_array[i][j] = random_number_in_0_1;
-                    System.out.print(map_array[i][j]);
-                    random_number_in_0_1 = rand.nextInt(2);
+                //    if ((position_in_row + block_offset) < map_width) {
+                            mapInstance.map_array[position_in_row][column_no] = random_number_in_0_1;
+                            System.out.print(map_array[position_in_row][column_no]);
+                            random_number_in_0_1 = rand.nextInt(2);
+                            if (column_no == 0 ) {counter_rows++;}
+                            position_in_row += block_offset;
+                //    }
                 }
-            }
-            j = 0;
-            System.out.println(" ");
-            i += block_offset;
+                position_in_row = 0;
+                System.out.println(" ");
+                column_no += block_offset;
+                counter_columns++;
+            //}
         }
+        System.out.println("number of objects in a row = " + counter_rows);
+        System.out.println("number of column of objects = " + counter_columns);
+        printArray(map_array);
+    }
 
+
+    private void printArray(int[][] mapArray) {
+        mapInstance.toString(mapArray);
     }
-    @Override
-    public String toString() {
-        return "not-null";
+
+
+    public String toString(int[][] map_array) {
+        return "Map{" +
+                "map_array=" + Arrays.toString(map_array) +
+                '}';
     }
+
     public static Map getMapInstance() {
         if (mapInstance == null) {
             mapInstance = new Map();
